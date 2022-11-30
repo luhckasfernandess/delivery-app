6a5c94b3edfb86a772452ab7106ec7263f861eaa
Vinicius <xxvi.paris@gmail.com>
2022-11-30 12:10:34 -0300

    feat: add tests of register page

--- front-end/src/tests/login.test.js
+++ front-end/src/tests/login.test.js
@@ -1,37 +1,32 @@
 import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './utils/renderWithRouter';
-import { screen } from "@testing-library/react";
-import userEvent from '@testing-library/user-event';
-
 
 describe('Testando a tela de login', () => {
+  const inputEmail = screen.getByTestId('common_login__input-email');
+  const inputPassword = screen.getByTestId('common_login__input-password');
+  const loginBtn = screen.getByTestId('common_login__button-login');
+  const singUpBtn = screen.getByTestId('common_login__button-login');
+
   it('Verifica se os inputs aparecem corretamente', () => {
     renderWithRouter(<App />);
-    const inputEmail = screen.getByTestId('common_login__input-email');
-    const inputPassword = screen.getByTestId('common_login__input-password');
-    const loginBtn = screen.getByTestId('common_login__button-login');
-    const singUpBtn = screen.getByTestId('common_login__button-login');
     expect(inputEmail).toBeInTheDocument();
     expect(inputPassword).toBeInTheDocument();
     expect(loginBtn).toBeInTheDocument();
+    expect(singUpBtn).toBeInTheDocument();
   });
   it(`Verifica se o botão de login está inicialmente desabilitado,
    e não se habilita passando dados incorretos`, () => {
-    const loginBtn = screen.getByTestId('common_login__button-login');
-    const inputEmail = screen.getByTestId('common_login__input-email');
-    const inputPassword = screen.getByTestId('common_login__input-password');
     expect(loginBtn).toBeDisabled();
     userEvent.type(inputEmail, 'invalidEmail');
     userEvent.type(inputPassword, '');
     expect(loginBtn).toBeDisabled();
-  })
+  });
   it('Verifica se o botão de login é habilitado quando passados dados corretos', () => {
-    const loginBtn = screen.getByTestId('common_login__button-login');
-    const inputEmail = screen.getByTestId('common_login__input-email');
-    const inputPassword = screen.getByTestId('common_login__input-password');
     userEvent.type(inputEmail, 'validEmail@email.com');
     userEvent.type(inputPassword, '123456');
     expect(loginBtn).not.toBeDisabled();
-  })
-})
+  });
+});
