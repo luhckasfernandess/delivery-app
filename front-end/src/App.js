bbc7f0b6c76956400a753470dc017a030ab48cce
Yuri Vasconcellos <y.vasconcellos@outlook.com>
2022-12-01 10:31:23 -0300

    feat: create admin page with mock

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Admin from './pages/Admin';
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={ <Login /> } />
       <Route path="/register" element={ <Register /> } />
+      <Route path="/admin/manage" element={ <Admin /> } />
       <Route path="/" element={ <Navigate to="/login" /> } />
     </Routes>
   );
