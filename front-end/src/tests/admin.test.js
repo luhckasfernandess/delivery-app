c641f78b5520a0fb46330df93e85e7946f29d63b
Vinicius <xxvi.paris@gmail.com>
2022-12-06 14:54:47 -0300

    feat: add test adm page

---
+++ front-end/src/tests/admin.test.js
@@ -0,0 +1,51 @@
+import { screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
+
+describe('testando a tela de administrador', () => {
+  const admInputName = 'admin_manage__input-name';
+  const admInputEmail = 'admin_manage__input-email';
+  const admInputPassword = 'admin_manage__input-password';
+  const admBtnRegister = 'common_register__button-register';
+  const route = 'admin/manage';
+  it('Verifica se todos os elementos aparecem corretamente na tela', () => {
+    render(
+      <MemoryRouter initialEntries={ [route] }>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const inputName = screen.getByTestId(admInputName);
+    const inputEmail = screen.getByTestId(admInputEmail);
+    const inputPassword = screen.getByTestId(admInputPassword);
+    const btnRegister = screen.getByTestId(admBtnRegister);
+
+    expect(inputName).toBeInTheDocument();
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputPassword).toBeInTheDocument();
+    expect(btnRegister).toBeInTheDocument();
+  });
+
+  it(`Verifica se o botão de cadastro esta inicialmente desabilitado,
+   e não se habilita passados dados incorretos`, () => {
+    render(
+      <MemoryRouter initialEntries={ [route] }>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const inputName = screen.getByTestId(admInputName);
+    const inputEmail = screen.getByTestId(admInputEmail);
+    const inputPassword = screen.getByTestId(admInputPassword);
+    const btnRegister = screen.getByTestId(admBtnRegister);
+    const invalidName = 'invalidName';
+
+    expect(btnRegister).toBeDisabled();
+
+    userEvent.type(inputName, invalidName);
+    userEvent.type(inputEmail, 'invalidEmail');
+    userEvent.type(inputPassword, '');
+
+    expect(btnRegister).toBeDisabled();
+  });
+});
