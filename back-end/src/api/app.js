cf131e6482907c18920d64689b86eb16326ebeea
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-12-08 21:01:32 -0400

    feat: cria uma rota estática para disponibilizar imagens dos produtos

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const UsersRouter = require('../routes/user.router');
 const ProductRouter = require('../routes/product.router');
 const SaleRouter = require('../routes/sale.router');
@@ -9,6 +10,7 @@ const AdminRouter = require('../routes/admin.router');
 
 const app = express();
 app.use(cors());
+app.use('/images', express.static(path.join(__dirname, '..', '..', 'images')));
 app.use(express.json());
 app.use(UsersRouter);
 app.use(AdminRouter);
