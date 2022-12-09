0b3238d56c0c7d1e4dd88aa8d07cbfa67a7f622b
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-12-08 22:19:53 -0400

    feat: adiciona função de incrementar e decrementar nos botões e atualiza o input

--- front-end/src/components/ProductsCard.jsx
+++ front-end/src/components/ProductsCard.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 export default function ProductsCard({ id, price, image, title }) {
+  const [counter, setCounter] = useState(0);
   return (
     <div>
       <div key={ id }>
@@ -23,16 +24,24 @@ export default function ProductsCard({ id, price, image, title }) {
         <button
           data-testid={ `customer_products__button-card-rm-item-${id}` }
           type="button"
+          onClick={ () => {
+            if (counter <= 0) return 0;
+            setCounter(counter - 1);
+          } }
         >
           -
         </button>
         <input
           data-testid={ `customer_products__input-card-quantity-${id}` }
-          value="0"
+          value={ counter }
+          onChange={ ({ target }) => {
+            if (Number(target.value)) setCounter(Number(target.value));
+          } }
         />
         <button
           data-testid={ `customer_products__button-card-add-item-${id}` }
           type="button"
+          onClick={ () => { setCounter(counter + 1); } }
         >
           +
         </button>
